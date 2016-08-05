feaa65476caef92da5ddc6631c3391add9b2f3df
Josue Gontijo <josue.eduardo206@gmail.com>
2016-08-05 01:10:28 +0100

    Minor update

--- batch-template/src/main/java/com/josue/batch/agent/core/ChunkExecutor.java
+++ batch-template/src/main/java/com/josue/batch/agent/core/ChunkExecutor.java
@@ -12,6 +12,7 @@ import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
 /**
  * @author Josue Gontijo
@@ -35,9 +36,7 @@ public abstract class ChunkExecutor {
         this.executor = config.getExecutor();
         this.provider = config.getInstanceProvider();
         this.meter = new Meter(config.isEnableMetrics());
-        for (Class<? extends ChunkListener> l : config.getListeners()) {
-            listenersDef.add(l);
-        }
+        listenersDef.addAll(config.getListeners().stream().collect(Collectors.toList()));
 
         logger.setLevel(config.getLogLevel());
     }
@@ -74,55 +73,52 @@ public abstract class ChunkExecutor {
         final String id = UUID.randomUUID().toString().substring(0, 8);
         final long jobStart = System.currentTimeMillis();
 
-        executor.submit(new Runnable() {
-            @Override
-            public void run() {
-                meter.start(MeterHint.TOTAL);
-                logger.log(Level.FINER, "Starting job, id: {0}, properties: {1}", new Object[]{id, props});
+        executor.submit((Runnable) () -> {
+            meter.start(MeterHint.TOTAL);
+            logger.log(Level.FINER, "Starting job, id: {0}, properties: {1}", new Object[]{id, props});
 
-                List<ChunkListener> listeners = new LinkedList<>();
-                try {
-                    for (Class<? extends ChunkListener> listener : listenersDef) {
-                        ChunkListener chunkListener = provider.newInstance(listener);
-                        meter.start(MeterHint.LISTENERINIT);
-                        chunkListener.init(props, meter);
-                        meter.end(MeterHint.LISTENERINIT);
-                        listeners.add(chunkListener);
-                    }
-
-                    //------ ONSTART ------
-                    meter.start(MeterHint.ONSTART);
-                    for (ChunkListener listener : listeners) {
-                        listener.onStart();
-                    }
-                    meter.end(MeterHint.ONSTART);
-
-                    //------ EXECUTE ------
-                    execute(id, props);
-
-                    //------ ONSUCCES ------
-                    meter.start(MeterHint.ONSUCCESS);
-                    for (ChunkListener listener : listeners) {
-                        listener.onSuccess();
-                    }
-                    meter.end(MeterHint.ONSUCCESS);
-
-                } catch (Exception ex) {
-                    //on error
-                    logger.log(Level.SEVERE, ex.getMessage(), ex);
-                    meter.start(MeterHint.ONERROR);
-                    for (ChunkListener listener : listeners) {
-                        try {
-                            listener.onFail(ex);
-                        } catch (Exception e) {
-                            logger.log(Level.SEVERE, ex.getMessage(), ex);
-                        }
-                    }
-                    meter.end(MeterHint.ONERROR);
+            List<ChunkListener> listeners = new LinkedList<>();
+            try {
+                for (Class<? extends ChunkListener> listener : listenersDef) {
+                    ChunkListener chunkListener = provider.newInstance(listener);
+                    meter.start(MeterHint.LISTENERINIT);
+                    chunkListener.init(props, meter);
+                    meter.end(MeterHint.LISTENERINIT);
+                    listeners.add(chunkListener);
                 }
-                logger.log(Level.FINER, "{0} - Finished in {1}ms", new Object[]{id, (System.currentTimeMillis() - jobStart)});
-                meter.end(MeterHint.TOTAL);
+
+                //------ ONSTART ------
+                meter.start(MeterHint.ONSTART);
+                for (ChunkListener listener : listeners) {
+                    listener.onStart();
+                }
+                meter.end(MeterHint.ONSTART);
+
+                //------ EXECUTE ------
+                execute(id, props);
+
+                //------ ONSUCCES ------
+                meter.start(MeterHint.ONSUCCESS);
+                for (ChunkListener listener : listeners) {
+                    listener.onSuccess();
+                }
+                meter.end(MeterHint.ONSUCCESS);
+
+            } catch (Exception ex) {
+                //on error
+                logger.log(Level.SEVERE, ex.getMessage(), ex);
+                meter.start(MeterHint.ONERROR);
+                for (ChunkListener listener : listeners) {
+                    try {
+                        listener.onFail(ex);
+                    } catch (Exception e) {
+                        logger.log(Level.SEVERE, ex.getMessage(), ex);
+                    }
+                }
+                meter.end(MeterHint.ONERROR);
             }
+            logger.log(Level.FINER, "{0} - Finished in {1}ms", new Object[]{id, (System.currentTimeMillis() - jobStart)});
+            meter.end(MeterHint.TOTAL);
         });
     }
 }
